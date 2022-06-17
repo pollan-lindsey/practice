5bb314f2984817444cef730a216ef74ff9421089
Lindsey Pollan <lpollan@live.com>
2022-06-16 23:23:53 -0500

    array work

---
+++ Arrays/app.js
@@ -0,0 +1,31 @@
+//EXCERCISEONE
+
+//need to use a function
+//set it to a function and attach the function to be run each press of the button?
+//make an array with four names
+const nameArray = ["Becki", "Alex", "Scott", "Lindsey"];
+//make the function with name nameFunction
+function nameFunction(){
+//set a variable for the popped array elements to go to
+let fourNames = nameArray.pop();
+//return the names in a way that doesn't make the button disappear
+document.getElementById("buttonOne").innerHTML = fourNames;
+}
+
+
+
+
+
+
+// console.log(names);
+// console.log(nameArray);
+// //write the element on the page
+// document.write(names);
+//write the name as it pops
+//what kind of loop do I use with this?
+
+//EXCERCISE TWO
+
+//make an array with 5 numbers
+//print the numbers one at a time
+//whatever I figure out to do in excercise one, bring that down here
---
+++ Constructors/index.js
@@ -0,0 +1,8 @@
+// function main(){
+//     let newDog = new Dog("Belle", "lab", "black", "female");
+//     alert(newDog.name);
+//     alert(newDog.breed);
+//     alert(newDog.color);
+//     alert(newDog.gender);
+//}
+//main();
